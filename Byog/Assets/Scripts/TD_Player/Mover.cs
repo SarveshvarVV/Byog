5173823574c9d6c2499f5c41291299725b7098ab
Sarvesh <sarvesh20304@gmail.com>
2023-09-15 22:26:44 +0530

    player

--- Byog/Assets/Scripts/TD_Player/Mover.cs
+++ Byog/Assets/Scripts/TD_Player/Mover.cs
@@ -1,20 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    private 
+    Rigidbody2D rb2d;
+    Vector2 moveInput;
+    [SerializeField] float moveSpeed = 5f;
+    bool canMove = true;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Movement();
+
+        }
+    }
+
+    private void Movement()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        rb2d.velocity = moveInput * moveSpeed * Time.deltaTime;
     }
 }
