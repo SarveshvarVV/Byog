b21f68959835b2fdb92733b7f9dab4d261a5ffb5
Sarvesh <sarvesh20304@gmail.com>
2023-09-16 03:17:45 +0530

    Update SS_CameraFollow.cs

--- Byog/Assets/Scripts/SS_Others/SS_CameraFollow.cs
+++ Byog/Assets/Scripts/SS_Others/SS_CameraFollow.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SS_CameraFollow : MonoBehaviour
 {
+
+    [SerializeField] Transform target;
+    [SerializeField] Transform leftBounds;
+    [SerializeField] Transform rightBounds;
+
+    [SerializeField] float smoothDampTime = 0.15f;
+    Vector3 smoothDampVelocity = Vector3.zero;
+
+    float camWidth, camHeight, levelMinX, levelMaxX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camHeight = Camera.main.orthographicSize * 2;
+        camWidth = camHeight * Camera.main.aspect;
+
+        float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x/2;
+        float rightBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+
+        levelMinX = leftBounds.position.x + leftBoundsWidth + (camWidth/2);
+        levelMaxX = rightBounds.position.x - rightBoundsWidth - (camWidth / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target)
+        {
+            float targetX = Mathf.Max(levelMinX, Mathf.Min(levelMaxX,target.position.x));
+            float x = Mathf.SmoothDamp(transform.position.x, targetX,ref smoothDampVelocity.x, smoothDampTime);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
     }
 }
