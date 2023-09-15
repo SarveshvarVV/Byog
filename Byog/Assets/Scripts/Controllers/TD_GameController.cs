89da648a836d9eddd3482032c97718884a06304b
Sarvesh <sarvesh20304@gmail.com>
2023-09-16 00:33:39 +0530

    blah

--- Byog/Assets/Scripts/Controllers/TD_GameController.cs
+++ Byog/Assets/Scripts/Controllers/TD_GameController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SGOAT_Movement : MonoBehaviour
+public class TD_GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
